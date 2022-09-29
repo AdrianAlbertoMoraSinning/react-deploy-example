9be0a7dfda7b056e22516a7257f3661173e7554b
Amosi137 <adrian.alberto.mora.sinning@gmail.com>
2022-09-28 20:58:08 -0500

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,16 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi tarea 1",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi tarea 2",
+  },{
+    id: 2,
+    title: "mi tercer tarea",
+    description: "mi tarea 3",
+  }
+];
